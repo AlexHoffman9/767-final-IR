b003c539c72542a488adab9c5ccb268e3efbe60a
AlexHoffman9 <alexhoffman21@gmail.com>
2020-04-12 18:34:52 -0400

    Basic IS agent running on randomwalk. Not correct yet

--- OffPolicyAgent.py
+++ OffPolicyAgent.py
@@ -7,75 +7,123 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.callbacks import EarlyStopping
 import tensorflow.keras.backend as K
 import matplotlib.pyplot as plt
-import random_walk_env as RW
+from random_walk_env import RandomWalkEnv
 import time
 
 
-#TODO: what is the target policy pi? need it to get importance ratio
-    # for epsilon greedy it is easy to compute by taking max of values and giving f
-    # their code mentions something about target policy. searching "target" and "Get_policy" in github yield results
-# 
-
-
+# Agent class with defaults for importance sampling
+# can write new classes for WIS, IR, etc. by overwriting key functions
+# Compatible with problem_name='RandomWalk' or 'FourRooms'
 class OffPolicyAgent():
     # construct agent's model separately, so it can be sized according to problem
-    def __init__(self, n_actions, n_replay, input_dim, out_dim, env, lr, discount):
-        self.replay_buffer = [0]*n_replay # change to data structure that stores s,a,r,s'
-        # self.model = self.build_model()
-        self.actions = range(n_actions)
+    def __init__(self, problem_name, n_replay, env, target_policy, behavior_policy, lr, discount):
         self.lr = lr
         self.discount = discount
-        self.model = self.build_model(input_dim, out_dim)
-        print(self.model.summary())
+        self.n_replay = n_replay
         self.env = env
+        if problem_name == 'RandomWalk':
+            self.actions = range(2)
+            self.n_features = 10  # fixed for 10 state randomwalk. just using sparse coding
+        elif problem_name == 'FourRooms':
+            self.actions = range(4)
+            self.n_features = 10 #TODO depends on tiling for states in fourroomsu. consult julia code. This needs to be input shape for the 2d tiled input
+        else:
+            print('ERRORRR: invalid problem name')
+        self.model = self.build_model(self.n_features, 1)
+        # print(self.model.summary())
+        self.target_policy = target_policy # 2d array indexed by state, action. example: 10x2 array for 10 state random walk
+        self.behavior_policy = behavior_policy
+        self.replay_buffer = np.zeros(shape=(n_replay), dtype=[('valid', bool), ('s',np.int32), ('s2',np.int32), ('r',np.int32), ('ratio', np.float)]) # state, next state, reward, ratio
+        self.t=0
 
     # build neural network for state value function
-    # I think a linear model would be plenty for the random walk example... so this could be changed
+    # Default is single layer linear layer
     def build_model(self, input_dim, out_dim):
         input_layer = Input(shape=(input_dim), name='state_input')
-        returns = Input(shape=(1), name='returns_input')
-        hidden_layer = Dense(32, activation = "relu", name='hidden_layer')(input_layer)
-        output_layer = Dense(out_dim, activation="softmax", name='output_layer')(hidden_layer)
+        # hidden_layer = Dense(32, activation = "relu", name='hidden_layer')(input_layer)
+        output_layer = Dense(out_dim, activation="linear", name='output_layer')(input_layer) #(hidden_layer)
         # loss function for batch update
-        opt = Adam(lr=self.lr, beta_1=0.9, beta_2=0.999, amsgrad=True)
+        # opt = Adam(lr=self.lr, beta_1=0.9, beta_2=0.999, amsgrad=True)
         model = Model(inputs=[input_layer], outputs=[output_layer])
-        model.compile(loss="mean_squared_error", optimizer = opt)
+        model.compile(loss="mean_squared_error", optimizer = SGD(lr=self.lr))
         return model
 
     # complete episode of experience and then train using buffer
     # not sure how much experience to get before training on it...one episode? 2? n timesteps?
-    def train(self):
-        buffer_index = 0
+    def generate_episode(self):
         # init state
         s = self.env.reset()
         done = False
         while not done:
             # choose action according to policy
-            a = np.random,choice(a=self.actions, p=self.policy(s))
+            a = np.random.choice(a=self.actions, p=self.behavior_policy[s])
             (s2,r,done,_) = env.step(a)
-            # compute the desired state value = r + v(s2)
-            #TODO: figure out what goes in the buffer, put it there
-                # likely faster to get all experience, then do batch of prediction to get v(s'), then put the data in the official replay buffer
-                # doing step->predict->add-to-buffer would be slow because of keras
-            # self.model.predict([s], )
-            # add example to buffer
-            self.replay_buffer[buffer_index,:]
-        # add 
-        
-    # need to choose tiling (will depend on child class for each environment
-    # default tiling is for 10 state random walk 
-    def construct_feature(self,s): 
-        return [i == s for i in range(self.len(self.actions))]
+            ratio = self.target_policy[s,a] / self.behavior_policy[s,a]
+            self.replay_buffer[self.t%self.n_replay] = (True,s,s2,r,ratio)
+            s=s2
+            self.t += 1
 
-    # uniform policy for now
-    # could replace this with specific agent
-    def policy(self, s):
-        n=len(self.actions)
-        return [1.0/n]*n
+    # do batch of training using replay buffer
+    # Default is to do a minibatch update. The paper uses both minibatch and incremental updates, so this could be changed
+    def train_batch(self, n_samples):
+        sample_indices = self.sample_buffer(n_samples)
+        # compute targets = ratio*(r + v(s'))
+        rewards = self.replay_buffer['r'][sample_indices]
+        # print("sample indices:", sample_indices)
+        next_state_features = self.construct_features(self.replay_buffer['s2'][sample_indices])
+        state_features = self.construct_features(self.replay_buffer['s'][sample_indices])
+        ratios = self.replay_buffer['ratio'][sample_indices]
+        next_values = self.model.predict(next_state_features).flatten()
+        # v(s') is zero for terminal state, so need to fix model prediction
+        for i in range(n_samples):
+            # if experience ends in terminal state, value function returns 0
+            if self.replay_buffer['s2'][sample_indices[i]]%self.n_features == 0: # this only works for randomwalk. 
+                next_values[i] = 0.0
+        targets = (rewards + self.discount*next_values)*ratios
+        # print("state features:", state_features)
+        # print('rewards:', rewards)
+        # print("ratios", ratios)
+        # print('next values:', next_values)
+        # print('targets',targets)
+        # train on samples
+        self.model.fit(state_features, targets, batch_size=n_samples, verbose=0)
+
+    # choose batch of experience from buffer. IR will change this function
+    # default: random n samples
+    def sample_buffer(self, n_samples):
+        max_index = self.n_replay
+        if self.t < self.n_replay:
+            max_index = self.t
+        return np.random.randint(low=0, high=max_index, size=n_samples, dtype=int)
+        # return [self.buffer_index-n_samples+i for i in range(n_samples)]
+
+    # need to choose tiling (will depend on child class for each environment
+    # default tiling is for 10 state random walk
+    def construct_features(self,states):
+        # print(states)
+        return np.array([[np.float(i == s) for i in range(self.n_features)] for s in states])
 
 
 then = time.time()
-env = RW.RandomWalkEnv(10)
-lr=.001
-discount=.99
-agent = OffPolicyAgent(2,1000,2,1,env, lr, discount)
+env = RandomWalkEnv(10)
+lr=.1
+discount=.5
+target=np.zeros(shape=(10,2), dtype=np.float)
+uniform_random_behavior=np.full(shape=(10,2), fill_value=0.5, dtype=np.float)
+# construct target policy: deterministic to the right
+for i in range(10):
+    target[i,1] = 1.0
+agent = OffPolicyAgent('RandomWalk', 128, env, target, uniform_random_behavior, lr, discount)
+# print out initial value function
+states = agent.construct_features(range(10))
+print(agent.model.predict(states))
+for j in range(10):
+    # generate 100 episodes, training after each
+    for i in range(100):
+        agent.generate_episode()
+        agent.train_batch(16)
+    # print current value function
+    states = agent.construct_features(range(10))
+    print(agent.model.predict(states))
+    
+
